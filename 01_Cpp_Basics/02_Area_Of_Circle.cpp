d15eaf25abfc9d06e6fdba44409ba2bf159bd603
kiran <kirankumar2003g@gmail.com>
2025-01-16 23:55:32 +0530

    Area of Circle

---
+++ 01_Cpp_Basics/02_Area_Of_Circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double R;
+    cin >> R;
+
+    double pie = 3.141592653;
+
+    cout << fixed << setprecision(9);
+
+    cout << pie * R * R;
+}
