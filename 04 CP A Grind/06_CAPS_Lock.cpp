ba27791df61f2da5f4e8d5e8037d2e9f391080c0
kiran <kirankumar2003g@gmail.com>
2025-02-16 11:10:06 +0530

    A Grind -> CAPS Lock

---
+++ 04 CP A Grind/06_CAPS_Lock.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int n = s.size();
+    for (int i = 0 ; i < n ; i++) {
+        s[i] = s[i] + 'A' - 'a';
+    }
+
+    cout << s << endl;
+}
+// Time -> O(N)
+// Space -> O(1)
