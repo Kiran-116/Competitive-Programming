8a4c8d9ff590fd66cfccbd1ba5a6df19eeb0aac0
kirankumar.g1 <kirankumar.g@sanas.ai>
2025-07-13 10:59:31 +0530

    Recursion and Backtracking - Apple Division

--- 06 Recursion and Backtracking/19_Apple_Division.cpp
+++ 06 Recursion and Backtracking/19_Apple_Division.cpp
@@ -1,14 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(vector<long long>& arr, int ind, long long l, long long r, long long &mini) {
-    if (ind == arr.size()) {
-        mini = min(mini, abs(l - r));
+// void solve(vector<long long>& arr, int ind, long long l, long long r, long long &mini) {
+//     if (ind == arr.size()) {
+//         mini = min(mini, abs(l - r));
+//         return;
+//     }
+
+//     solve(arr, ind+1, l + arr[ind], r, mini);
+//     solve(arr, ind+1, l, r + arr[ind], mini);
+// }
+
+// Approach - 2: Take, Not Take Pattern:
+void solve2(vector<long long>& arr, int ind, long long sum, long long totalSum, int n, long long &mini) {
+    if (ind == n) {
+        mini = min(mini, abs(sum - (totalSum - sum)));
         return;
     }
 
-    solve(arr, ind+1, l + arr[ind], r, mini);
-    solve(arr, ind+1, l, r + arr[ind], mini);
+    // Include the current element in the left partition
+    solve2(arr, ind + 1, sum + arr[ind], totalSum, n, mini); 
+    solve2(arr, ind + 1, sum, totalSum, n, mini);
 }
 
 int main()
@@ -23,7 +35,9 @@ int main()
 
     long long mini = LLONG_MAX;
 
-    solve(arr, 0, 0, 0, mini);
+    // solve(arr, 0, 0, 0, mini);
+    long long totalSum = accumulate(arr.begin(), arr.end(), 0LL);
+    solve2(arr, 0, 0, totalSum, arr.size(), mini);
 
     cout << mini << endl;
 
